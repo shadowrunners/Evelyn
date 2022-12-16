17eea261784ddd2c8da01a1852030c8a6d04c573
scrappie <scr3ppie@gmail.com>
2022-12-16 16:32:13 +0200

    refactor: WaifuWrapper: Enhanced Edition

--- src/functions/waifuAPI.js
+++ src/functions/waifuAPI.js
@@ -21,9 +21,16 @@ module.exports = class WaifuEngine {
   }
 
   /** Retrieves the image from the endpoint provided. */
-  async fetchImage(endpoint) {
-    const { body } = await get(`${this.apiURL}${endpoint}`);
-    return body.url;
+  fetchImage(endpoint) {
+    return new Promise((resolve, reject) => {
+      get(`${this.apiURL}${endpoint}`)
+        .then((res) => {
+          resolve(res.body.url);
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
   }
 
   /** Checks for a target user to display in the embed whenever a person needs to be mentioned. */
@@ -33,275 +40,109 @@ module.exports = class WaifuEngine {
     if (!this.target)
       return this.interaction.editReply({
         embeds: [
-          new EmbedBuilder().setDescription(
-            "🔹 | You forgot to provide a user."
-          ),
+          new EmbedBuilder()
+            .setColor("Blurple")
+            .setDescription("🔹 | You forgot to provide a user."),
         ],
-        ephemeral: true,
       });
+  }
+
+  /** Fetches the image and */
+  async reply(action, text, target) {
+    if (target) {
+      if (this.checkTarget(target)) return;
+    }
+
+    const image = await this.fetchImage(action);
+    let name, iconURL;
+
+    if (target) {
+      name = `${this.interaction.user.username} ${text} ${target.username}`;
+      iconURL = `${this.interaction.user.avatarURL()}`;
+    } else {
+      name = `${this.interaction.user.username} ${text}`;
+      iconURL = `${this.interaction.user.avatarURL()}`;
+    }
+
+    return this.interaction.editReply({
+      embeds: [
+        this.embed.setAuthor({ name: name, iconURL: iconURL }).setImage(image),
+      ],
+    });
   }
 
   /** Fetches the biting images from the API and replies with an embed of it. */
-  async bite(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("bite").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} bites ${target.username}`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  bite(target) {
+    this.reply("bite", "bites", target);
   }
 
   /** Fetches blushing images from the API and replies with an embed of it. */
-  async blush() {
-    await this.fetchImage("blush").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} blushes`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  blush() {
+    this.reply("blush", "blushes");
   }
 
   /** Fetches bonk images from the API and replies with an embed of it. */
-  async bonk(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("bonk").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} bonks ${target.username}`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  bonk(target) {
+    this.reply("bonk", "bonks", target);
   }
 
   /** Fetches bully images from the API and replies with an embed of it. */
-  async bully(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("bully").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} bullies ${target.username}`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  bully(target) {
+    this.reply("bully", "bullies", target);
   }
 
   /** Fetches cringe images from the API and replies with an embed of it. */
-  async cringe() {
-    await this.fetchImage("cringe").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} thinks that's pretty cringe`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  cringe() {
+    this.reply("cringe", "thinks that's pretty cringe");
   }
 
   /** Fetches crying images from the API and replies with an embed of it. */
-  async cry() {
-    await this.fetchImage("cry").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} is crying :c`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  cry() {
+    this.reply("cry", "is crying... :c");
   }
 
   /** Fetches cuddling images from the API and replies with an embed of it. */
-  async cuddle(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("cuddle").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} cuddles ${target.username}`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  cuddle(target) {
+    this.reply("cuddle", "cuddles", target);
   }
 
   /** Fetches handholding images from the API and replies with an embed of it. */
-  async handhold(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("handhold").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} is holding ${target.username}'s hand`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  handhold(target) {
+    this.reply("handhold", "is holding hands with", target);
   }
 
   /** Fetches highfive images from the API and replies with an embed of it. */
-  async highfive(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("highfive").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} highfives ${target.username}`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  highfive(target) {
+    this.reply("highfive", "highfives", target);
   }
 
   /** Fetches hugging images from the API and replies with an embed of it. */
-  async hug(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("hug").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} hugs ${target.username}`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  hug(target) {
+    this.reply("hug", "hugs", target);
   }
 
   /** Fetches kissing images from the API and replies with an embed of it. */
-  async kiss(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("kiss").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} kisses ${target.username}`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  kiss(target) {
+    this.reply("kiss", "kisses", target);
   }
 
   /** Fetches patting images from the API and replies with an embed of it. */
-  async pat(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("pat").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} pats ${target.username}`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  pat(target) {
+    this.reply("pat", "pats", target);
   }
 
   /** Fetches poking images from the API and replies with an embed of it. */
-  async poke(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("poke").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} blushes`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  poke(target) {
+    this.reply("poke", "pokes", target);
   }
 
   /** Fetches slapping images from the API and replies with an embed of it. */
-  async slap(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("slap").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} slaps ${target.username}`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  slap(target) {
+    this.reply("slap", "slaps", target);
   }
 
   /** Fetches waving images from the API and replies with an embed of it. */
-  async wave(target) {
-    if (this.checkTarget(target)) return;
-
-    await this.fetchImage("wave").then((image) => {
-      return this.interaction.editReply({
-        embeds: [
-          this.embed
-            .setAuthor({
-              name: `${this.interaction.user.username} waves at ${target.username}`,
-              iconURL: this.interaction.user.avatarURL({ dynamic: true }),
-            })
-            .setImage(image),
-        ],
-      });
-    });
+  wave(target) {
+    this.reply("waves", "is waving at", target);
   }
 };
