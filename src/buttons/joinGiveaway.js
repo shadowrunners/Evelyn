6ae4c562cbc856aeafcec65c904da12d54fd52f1
scrappie <scr3ppie@gmail.com>
2023-01-11 16:56:55 +0200

    fix: resolved more eslint errors

--- src/buttons/joinGiveaway.js
+++ src/buttons/joinGiveaway.js
@@ -1,52 +1,55 @@
-const { ButtonInteraction, EmbedBuilder } = require("discord.js");
-const DB = require("../structures/schemas/giveaway.js");
+// eslint-disable-next-line no-unused-vars
+const { ButtonInteraction, EmbedBuilder } = require('discord.js');
+const DB = require('../structures/schemas/giveaway.js');
 
 module.exports = {
-  id: "joinGiveaway",
-  /**
-   * @param {ButtonInteraction} interaction
-   */
-  async execute(interaction) {
-    const embed = new EmbedBuilder().setColor("Blurple").setTimestamp();
+	id: 'joinGiveaway',
+	/**
+	 * @param {ButtonInteraction} interaction
+	 */
+	async execute(interaction) {
+		const { guild, channel, message, user } = interaction;
 
-    const data = await DB.findOne({
-      guildId: interaction.guild.id,
-      channelId: interaction.channel.id,
-      messageId: interaction.message.id,
-    });
+		const embed = new EmbedBuilder().setColor('Blurple').setTimestamp();
 
-    if (!data) {
-      embed.setDescription("🔹 | There is no data in the database.");
-      return interaction.reply({ embeds: [embed], ephemeral: true });
-    }
+		const data = await DB.findOne({
+			guildId: guild.id,
+			channelId: channel.id,
+			messageId: message.id,
+		});
 
-    if (data.enteredUsers.includes(interaction.user.id)) {
-      embed.setDescription("🔹 | You have already joined the giveaway.");
-      return interaction.reply({ embeds: [embed], ephemeral: true });
-    }
+		if (!data) {
+			embed.setDescription('🔹 | There is no data in the database.');
+			return interaction.reply({ embeds: [embed], ephemeral: true });
+		}
 
-    if (data.isPaused === true) {
-      embed.setDescription("🔹 | This giveaway is currently paused.");
-      return interaction.reply({ embeds: [embed], ephemeral: true });
-    }
+		if (data.enteredUsers.includes(user.id)) {
+			embed.setDescription('🔹 | You have already joined the giveaway.');
+			return interaction.reply({ embeds: [embed], ephemeral: true });
+		}
 
-    if (data.hasEnded === true) {
-      embed.setDescription("🔹 | Unfortunately, this giveaway has ended.");
-      return interaction.reply({ embeds: [embed], ephemeral: true });
-    }
+		if (data.isPaused === true) {
+			embed.setDescription('🔹 | This giveaway is currently paused.');
+			return interaction.reply({ embeds: [embed], ephemeral: true });
+		}
 
-    await DB.findOneAndUpdate(
-      {
-        guildId: interaction.guild.id,
-        channelId: interaction.channel.id,
-        messageId: interaction.message.id,
-      },
-      {
-        $push: { enteredUsers: interaction.user.id },
-      }
-    ).then(() => {
-      embed.setDescription("🔹 | Your entry has been confirmed. Good luck!");
-      return interaction.reply({ embeds: [embed], ephemeral: true });
-    });
-  },
+		if (data.hasEnded === true) {
+			embed.setDescription('🔹 | Unfortunately, this giveaway has ended.');
+			return interaction.reply({ embeds: [embed], ephemeral: true });
+		}
+
+		await DB.findOneAndUpdate(
+			{
+				guildId: guild.id,
+				channelId: channel.id,
+				messageId: message.id,
+			},
+			{
+				$push: { enteredUsers: user.id },
+			},
+		).then(() => {
+			embed.setDescription('🔹 | Your entry has been confirmed. Good luck!');
+			return interaction.reply({ embeds: [embed], ephemeral: true });
+		});
+	},
 };
--- src/commands/context/avatar.js
+++ src/commands/context/avatar.js
@@ -1,31 +1,32 @@
 const {
-  EmbedBuilder,
-  ApplicationCommandType,
-  ContextMenuCommandBuilder,
-  UserContextMenuCommandInteraction,
-} = require("discord.js");
+	EmbedBuilder,
+	ApplicationCommandType,
+	ContextMenuCommandBuilder,
+	// eslint-disable-next-line no-unused-vars
+	UserContextMenuCommandInteraction,
+} = require('discord.js');
 
 module.exports = {
-  botPermissions: ["SendMessages"],
-  data: new ContextMenuCommandBuilder()
-    .setName("User Avatar")
-    .setType(ApplicationCommandType.User),
-  /**
-   * @param {UserContextMenuCommandInteraction} interaction
-   */
-  async execute(interaction) {
-    const target = await interaction.guild.members.fetch(interaction.targetId);
-    await target.user.fetch();
+	botPermissions: ['SendMessages'],
+	data: new ContextMenuCommandBuilder()
+		.setName('User Avatar')
+		.setType(ApplicationCommandType.User),
+	/**
+	 * @param {UserContextMenuCommandInteraction} interaction
+	 */
+	async execute(interaction) {
+		const target = await interaction.guild.members.fetch(interaction.targetId);
+		await target.user.fetch();
 
-    return interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setColor("Blurple")
-          .setTitle(`${target.user.tag}'s Avatar`)
-          .setImage(target.user.avatarURL({ dynamic: true, size: 2048 }))
-          .setURL(target.avatarURL()),
-      ],
-      ephemeral: true,
-    });
-  },
+		return interaction.reply({
+			embeds: [
+				new EmbedBuilder()
+					.setColor('Blurple')
+					.setTitle(`${target.user.tag}'s Avatar`)
+					.setImage(target.user.avatarURL({ dynamic: true, size: 2048 }))
+					.setURL(target.avatarURL()),
+			],
+			ephemeral: true,
+		});
+	},
 };
