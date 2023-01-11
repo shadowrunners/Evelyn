d24c758d79a968204f577535bc40e3c4e3380daf
scrappie <scr3ppie@gmail.com>
2023-01-11 16:56:09 +0200

    fix: crash reporter now dumps the full error

--- src/functions/crashReport.js
+++ src/functions/crashReport.js
@@ -1,25 +1,30 @@
 /** This function manages Evelyn's Crash Report function which will send any errors to your Overwatch channel. */
-const { WebhookClient, EmbedBuilder, codeBlock } = require("discord.js");
+const { WebhookClient, EmbedBuilder, codeBlock } = require('discord.js');
+const { inspect } = require('util');
 
 function crashReporter(client, err) {
-  if (!client.config.debug.watcherHook) return;
+	if (!client.config.debug.watcherHook) return;
 
-  const crashReportClient = new WebhookClient({
-    url: client.config.debug.watcherHook,
-  });
+	const crashReportClient = new WebhookClient({
+		url: client.config.debug.watcherHook,
+	});
 
-  const embed = new EmbedBuilder()
-    .setColor("Red")
-    .setTitle(`Crash Report`)
-    .setDescription(
-      `\n${codeBlock(`- CRASH DUMP START -\n\n${err}\n\n- CRASH DUMP END -`)} `
-    )
-    .setTimestamp();
+	const embed = new EmbedBuilder()
+		.setColor('Red')
+		.setTitle('Crash Report')
+		.setDescription(
+			`\n${codeBlock(
+				`- CRASH DUMP START -\n\n${inspect(err, {
+					depth: 0,
+				})}\n\n- CRASH DUMP END -`,
+			)} `,
+		)
+		.setTimestamp();
 
-  return crashReportClient.send({
-    embeds: [embed],
-    content: `<@${client.config.ownerIDs}>`,
-  });
+	return crashReportClient.send({
+		embeds: [embed],
+		content: `<@${client.config.ownerIDs}>`,
+	});
 }
 
 module.exports = { crashReporter };
