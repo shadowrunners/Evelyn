b87d290808f79d07ff0ad5516cc61cc15628d67b
scrappie <scr3ppie@gmail.com>
2023-01-13 23:27:46 +0200

    refactor: optimize voice check for music

--- src/events/voice/peepsCheck.js
+++ src/events/voice/peepsCheck.js
@@ -1,60 +1,29 @@
-const { VoiceState, EmbedBuilder } = require("discord.js");
-const client = require("../../structures/index.js");
+const { EmbedBuilder } = require('discord.js');
 
 module.exports = {
-  name: "voiceStateUpdate",
-  /**
-   * @param {VoiceState} oldState
-   * @param {VoiceState} newState
-   */
-  execute(oldState, newState) {
-    const player = client.manager.players.get(oldState.guild.id);
-    if (player && !newState.guild.members.me.voice.channel) player.destroy();
+	name: 'voiceStateUpdate',
+	execute(oldState, newState, client) {
+		const player = client.manager.players.get(oldState.guild.id);
 
-    if (isUserLeavingVoiceChannel(oldState)) {
-      if (AmIAlone(oldState)) {
-        if (shouldILeave(oldState)) {
-          savePerformance(player, oldState, newState);
-        }
-      }
-    }
-  },
+		if (player && !newState.guild.members.me.voice.channel) player.destroy();
+
+		const getMyVCId = oldState.channelId === oldState.guild.members.cache.get(client.user.id).voice.channelId;
+		if (!getMyVCId) return;
+
+		const usersConnected = oldState.guild.members.me.voice.channel?.members.size;
+		if (usersConnected === 1) {
+			setTimeout(() => {
+				player?.destroy();
+
+				const textChannel = oldState.guild.channels.cache.get(player?.textId);
+				textChannel?.send({
+					embeds: [
+						new EmbedBuilder()
+							.setColor('Blurple')
+							.setDescription('I have left your VC due to it being empty to save resources.'),
+					],
+				});
+			});
+		}
+	},
 };
-
-function isUserLeavingVoiceChannel(state) {
-  const { channelId } = state.guild.members.cache.get(client.user.id).voice;
-  return channelId === state.channelId;
-}
-
-function AmIAlone(state) {
-  const { channel } = state.guild.members.me.voice;
-  if (!channel) return false;
-
-  const nonBotMembers = channel.members.filter((m) => !m.user.bot);
-  return nonBotMembers.size === 0;
-}
-
-function shouldILeave(state) {
-  const members = state.guild.members.me.voice.channel?.members.size;
-  return !members || members === 1;
-}
-
-function savePerformance(player, oldState, newState) {
-  setTimeout(() => {
-    const guild = client.guilds.cache.get(newState.guild.id);
-    const textChannel = guild.channels.cache.get(player?.textId);
-
-    const { channel } = oldState.guild.members.me.voice;
-    if (channel) {
-      player.destroy();
-      textChannel?.send({
-        embeds: [
-          new EmbedBuilder()
-            .setColor("Blurple")
-            .setDescription("🔹 | I left your VC to save resources.")
-            .setTimestamp(),
-        ],
-      });
-    }
-  }, 30000);
-}
