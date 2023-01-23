68640c520a8ddc8548f673d1c0ac85c98244fa37
scrappie <scr3ppie@gmail.com>
2023-01-23 22:01:24 +0200

    feat: added event for reaction roles

---
+++ src/events/interaction/reactionRoles.js
@@ -0,0 +1,27 @@
+const { StringSelectMenuInteraction } = require('discord.js');
+
+module.exports = {
+    name: 'interactionCreate',
+    /**
+     * @param {StringSelectMenuInteraction} interaction
+     */
+    execute(interaction) {
+        if (!interaction.isStringSelectMenu()) return;
+
+        const { member, customId, values, component } = interaction;
+
+        if (customId == "reaction") {
+            component.data.options.forEach(option => {
+                if (!values.includes(option.value) && member.roles.cache.has(option.value)) {
+                    member.roles.remove(option.value);
+                }
+            });
+
+            values.forEach(id => {
+                if (!member.roles.cache.has(id)) member.roles.add(id);
+            })
+
+            interaction.reply({ content: "Your roles have been updated.", ephemeral: true });
+        };
+    }
+}
