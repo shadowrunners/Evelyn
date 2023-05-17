b51235e1b0f6b336376e787024258f76b93cf20c
scrappie <scr3ppie@gmail.com>
2023-05-17 22:14:51 +0300

    refactor: ported all image commands

--- src/Commands/Image/cat.ts
+++ src/Commands/Image/cat.ts
@@ -1,33 +1,30 @@
-import {
-	SlashCommandBuilder,
-	ChatInputCommandInteraction,
-	PermissionFlagsBits,
-	EmbedBuilder,
-} from 'discord.js';
-import { Command } from '../../Interfaces/interfaces.js';
-import { Evelyn } from '../../structures/Evelyn.js';
+import { ChatInputCommandInteraction, EmbedBuilder } from 'discord.js';
+import { Discord, Slash } from 'discordx';
+import { Evelyn } from '../../Evelyn.js';
 import { get } from 'superagent';
 
-const { SendMessages, EmbedLinks } = PermissionFlagsBits;
+@Discord()
+export class Catto {
+	private embed: EmbedBuilder;
 
-const command: Command = {
-	botPermissions: [SendMessages, EmbedLinks],
-	data: new SlashCommandBuilder()
-		.setName('cat')
-		.setDescription('Shows you a random cat picture.'),
-	async execute(interaction: ChatInputCommandInteraction, client: Evelyn) {
+	constructor() {
+		this.embed = new EmbedBuilder()
+			.setColor('Blurple')
+			.setFooter({ text: 'Powered by TheCatAPI' })
+			.setTimestamp();
+	}
+
+	@Slash({
+		description: 'Shows you a random cat picture.',
+		name: 'cat',
+	})
+	async cat(interaction: ChatInputCommandInteraction, client: Evelyn) {
 		const { body } = await get(
 			'https://api.thecatapi.com/v1/images/search',
 		).set('x-api-key', client.config.APIs.cattoKey);
-		return interaction.reply({
-			embeds: [
-				new EmbedBuilder()
-					.setColor('Blurple')
-					.setImage(body[0].url)
-					.setTimestamp(),
-			],
-		});
-	},
-};
 
-export default command;
+		return interaction.reply({
+			embeds: [this.embed.setImage(body[0].url)],
+		});
+	}
+}
