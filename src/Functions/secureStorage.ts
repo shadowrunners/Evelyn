2f8bd080a2718157d1a615a675a98e9640a526cf
scrappie <scr3ppie@gmail.com>
2023-06-04 15:18:50 +0300

    feat: add secure storage module

---
+++ src/Functions/secureStorage.ts
@@ -0,0 +1,20 @@
+import cryptr from 'cryptr';
+import { Evelyn } from '../Evelyn.js';
+
+export function encryptMyData(value: string, client: Evelyn) {
+	const keepMyDataSecure = new cryptr(client.config.decryptionKey, {
+		pbkdf2Iterations: 15000,
+		saltLength: 15,
+	});
+
+	return keepMyDataSecure.encrypt(value);
+}
+
+export function pleaseDecryptMyData(encryptedValue: string, client: Evelyn) {
+	const keepMyDataSecure = new cryptr(client.config.decryptionKey, {
+		pbkdf2Iterations: 15000,
+		saltLength: 15,
+	});
+
+	return keepMyDataSecure.decrypt(encryptedValue);
+}
