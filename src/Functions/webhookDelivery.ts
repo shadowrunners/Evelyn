bd7bd7eb377530bc6b00b61ac9584661def4904d
scrappie <scr3ppie@gmail.com>
2023-06-04 15:19:25 +0300

    feat: decrypt webhook token when needed

--- src/Functions/webhookDelivery.ts
+++ src/Functions/webhookDelivery.ts
@@ -1,28 +1,54 @@
 import { WebhookClient, EmbedBuilder } from 'discord.js';
+import { pleaseDecryptMyData } from './secureStorage.js';
+import { Evelyn } from '../Evelyn.js';
 
 /** This function delivers the logs using Discord Webhooks. It exists as a separate dedicated function to avoid repeating code. */
-export function webhookDelivery(type: string, data: any, embed: EmbedBuilder) {
-	if (data?.type?.webhook !== Object) return;
+export function webhookDelivery(
+	type: string,
+	data: any,
+	embed: EmbedBuilder,
+	client: Evelyn,
+): void {
+	// if (data?.type?.webhook !== Object) return;
 
-	const logsDropOff = new WebhookClient({
-		id: data?.logs?.webhook?.id,
-		token: data?.logs?.webhook?.token,
-	});
+	const handleWebhooks = () => {
+		if (type === 'logs') {
+			console.log('Type detected as logs!');
+			const decryptedToken = pleaseDecryptMyData(
+				data?.logs?.webhook.token,
+				client,
+			);
 
-	const confessDropOff = new WebhookClient({
-		id: data?.confessions?.webhook?.id,
-		token: data?.confessions?.webhook?.token,
-	});
+			console.log(`Decrypted webhook token: ${decryptedToken}`);
 
-	switch (type) {
-	case 'logs':
-		return logsDropOff.send({
-			embeds: [embed],
-		});
+			const logsDropOff = new WebhookClient({
+				id: data?.logs?.webhook?.id,
+				token: decryptedToken,
+			});
 
-	case 'confessions':
-		return confessDropOff.send({
-			embeds: [embed],
-		});
-	}
+			console.log(`Created new logs drop off client: ${logsDropOff}`);
+
+			return logsDropOff.send({
+				embeds: [embed],
+			});
+		}
+
+		if (type === 'confessions') {
+			const decryptedToken = pleaseDecryptMyData(
+				data?.confessions?.webhook.token,
+				client,
+			);
+
+			const confessDropOff = new WebhookClient({
+				id: data?.confessions?.webhook?.id,
+				token: decryptedToken,
+			});
+
+			return confessDropOff.send({
+				embeds: [embed],
+			});
+		}
+	};
+
+	if (type) handleWebhooks();
 }
