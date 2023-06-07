1aece8717081037f0327eee77ec7aa7606cf3a73
scrappie <scr3ppie@gmail.com>
2023-06-07 16:07:38 +0300

    refactor: reduce code repetition + extra
    - Update reduces code repetition by merging user and message context handlers and buttons.
    - It also removes discriminators as a part of the username change.

--- src/Commands/Context/user.ts
+++ src/Commands/Context/user.ts
@@ -12,28 +12,31 @@ import {
 } from 'discord.js';
 import { Util } from '../../Modules/Utils/utils.js';
 
+const { User, Message } = ApplicationCommandType;
+
 @Discord()
 export class UserInformation {
-	private embed: EmbedBuilder;
 	private util: Util;
 
-	constructor() {
-		this.embed = new EmbedBuilder().setColor('Blurple').setTimestamp();
+	@ContextMenu({ name: 'User Information', type: User })
+	@ContextMenu({ name: 'User Information', type: Message })
+	async userInfo(
+		interaction:
+			| UserContextMenuCommandInteraction
+			| MessageContextMenuCommandInteraction,
+	) {
+		const { guild, targetId } = interaction;
+		const target = await guild.members.fetch(targetId);
+		const { user, presence, joinedTimestamp } = target;
+		const embed = new EmbedBuilder().setColor('Blurple').setTimestamp();
+
 		this.util = new Util();
-	}
 
-	@ContextMenu({
-		name: 'User Information',
-		type: ApplicationCommandType.User,
-	})
-	async userHandler(
-		interaction: UserContextMenuCommandInteraction,
-	): Promise<void> {
-		const { guild, targetId } = interaction;
-		const target = await guild.members.fetch(targetId);
-		const { user, presence, joinedTimestamp } = target;
-
-		await user.fetch();
+		await Promise.all([
+			user.fetch(),
+			user.avatarURL(),
+			user.bannerURL({ size: 512 }),
+		]);
 
 		const createdTime = this.util.convertToUnixTimestamp(user.createdTimestamp);
 		const joinedTime = this.util.convertToUnixTimestamp(joinedTimestamp);
@@ -52,11 +55,11 @@ export class UserInformation {
 				.setStyle(ButtonStyle.Primary),
 		);
 
-		interaction.reply({
+		return interaction.reply({
 			embeds: [
-				this.embed
+				embed
 					.setAuthor({
-						name: `${user.tag}`,
+						name: `${user.username}`,
 						iconURL: `${user.avatarURL()}`,
 					})
 					.setThumbnail(user.avatarURL())
@@ -94,119 +97,34 @@ export class UserInformation {
 			components: [actionRow],
 			ephemeral: true,
 		});
-
-		return;
-	}
-
-	@ContextMenu({
-		name: 'User Information',
-		type: ApplicationCommandType.Message,
-	})
-	async messageHandler(
-		interaction: MessageContextMenuCommandInteraction,
-	): Promise<void> {
-		const { guild, targetId } = interaction;
-		const target = await guild.members.fetch(targetId);
-		const { user, presence, joinedTimestamp } = target;
-
-		await user.fetch();
-
-		const createdTime = this.util.convertToUnixTimestamp(user.createdTimestamp);
-		const joinedTime = this.util.convertToUnixTimestamp(joinedTimestamp);
-		const mappedActivities = presence?.activities?.map(
-			(activity) => `${ActivityType[activity.type]} ${activity.name}`,
-		);
-
-		const actionRow = new ActionRowBuilder<ButtonBuilder>().setComponents(
-			new ButtonBuilder()
-				.setCustomId('avatar')
-				.setLabel('Avatar')
-				.setStyle(ButtonStyle.Primary),
-			new ButtonBuilder()
-				.setCustomId('banner')
-				.setLabel('Banner')
-				.setStyle(ButtonStyle.Primary),
-		);
-
-		interaction.reply({
-			embeds: [
-				this.embed
-					.setAuthor({
-						name: `${user.tag}`,
-						iconURL: `${user.avatarURL()}`,
-					})
-					.setThumbnail(user.avatarURL())
-					.setImage(user.bannerURL({ size: 512 }) || null)
-					.addFields(
-						{
-							name: 'General',
-							value: [
-								`> **Name** ${user.username}`,
-								`> **ID** ${user.id}`,
-								`> **Discriminator** #${user.discriminator}`,
-								`> **Discord member since** <t:${createdTime}:R>`,
-								`> **Server member since** <t:${joinedTime}:R>`,
-							].join('\n'),
-						},
-						{
-							name: 'Activity',
-							value: [
-								`> **Status** ${presence.status}`,
-								`> **Current Activity** ${
-									mappedActivities?.join(', ') ?? 'Nothing.'
-								}`,
-							].join('\n'),
-						},
-						{
-							name: 'Roles',
-							value:
-								`> ${target.roles.cache
-									.map((r) => r)
-									.join(' ')
-									.replace('@everyone', '')}` || 'None',
-						},
-					),
-			],
-			components: [actionRow],
-			ephemeral: true,
-		});
-
-		return;
 	}
 
 	@ButtonComponent({ id: 'avatar' })
-	async avatarHandler(interaction: ButtonInteraction): Promise<void> {
-		const { user } = interaction;
-		await user.fetch();
-
-		interaction.reply({
-			embeds: [
-				this.embed
-					.setTitle(`${user.tag}'s Avatar`)
-					.setImage(user.avatarURL({ size: 4096 }))
-					.setURL(user.avatarURL({ size: 4096 })),
-			],
-			ephemeral: true,
-		});
-
-		return;
-	}
-
 	@ButtonComponent({ id: 'banner' })
-	async bannerHandler(interaction: ButtonInteraction): Promise<void> {
-		const { user } = interaction;
+	async buttonHandler(interaction: ButtonInteraction) {
+		const { user, customId } = interaction;
+		const embed = new EmbedBuilder().setColor('Blurple').setTimestamp();
+
 		await user.fetch();
 
-		interaction.reply({
+		return interaction.reply({
 			embeds: [
-				this.embed
-					.setTitle(`${user.tag}'s Banner`)
-					.setImage(user.bannerURL({ size: 4096 }))
-					.setURL(user.bannerURL({ size: 4096 })),
+				embed
+					.setTitle(
+						`${user.username}'s ${customId === 'avatar' ? 'Avatar' : 'Banner'}`,
+					)
+					.setImage(
+						user[customId === 'avatar' ? 'avatarURL' : 'bannerURL']({
+							size: 4096,
+						}),
+					)
+					.setURL(
+						user[customId === 'avatar' ? 'avatarURL' : 'bannerURL']({
+							size: 4096,
+						}),
+					),
 			],
 			ephemeral: true,
 		});
-
-		return;
 	}
 }
