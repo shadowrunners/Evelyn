889ee249055ad2f3a1dce7b9f78e5035b03f77e8
scrappie <scr3ppie@gmail.com>
2023-06-07 15:38:08 +0300

    refactor: import TS files from src folder

--- src/Functions/fileLoader.ts
+++ src/Functions/fileLoader.ts
@@ -1,20 +1,13 @@
 import { glob } from 'glob';
-import { join, resolve, extname } from 'path';
-
-/** Deletes the cached file. */
-function deleteCachedFile(file: string) {
-	const filePath = resolve(file);
-	if (require.cache[filePath]) delete require.cache[filePath];
-}
+import { join, extname } from 'path';
 
 /** Loads the files from the provided directory. */
 export async function fileLoad(dir: string) {
 	try {
 		const files = await glob(
-			join(`${process.cwd()}/dist/`, dir, '**/*.js').replace(/\\/g, '/'),
+			join(`${process.cwd()}/src/`, dir, '**/*.ts').replace(/\\/g, '/'),
 		);
-		const jsFiles = files.filter((file) => extname(file) === '.js');
-		await Promise.all(jsFiles.map(deleteCachedFile));
+		const jsFiles = files.filter((file) => extname(file) === '.ts');
 		return jsFiles;
 	}
 	catch (err) {
