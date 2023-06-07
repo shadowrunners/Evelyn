25554d366b2b2d334cec43d45d0afa7f747a767d
scrappie <scr3ppie@gmail.com>
2023-06-07 16:14:22 +0300

    refactor: remove usertag

--- src/Functions/replacePlaceholders.ts
+++ src/Functions/replacePlaceholders.ts
@@ -1,11 +1,10 @@
 import { GuildMember } from 'discord.js';
 
-/** This function replaces the tags (i.e: ${userTag}) used in the welcome and goodbye systems. */
+/** This function replaces the tags (i.e: ${userID}) used in the welcome and goodbye systems. */
 export function replacePlaceholders(string: string, member: GuildMember) {
 	const { user, guild } = member;
 
 	return string
-		?.replace(/{userTag}/g, user.tag)
 		?.replace(/{userName}/g, user.username)
 		?.replace(/{userID}/g, user.id)
 		?.replace(/{userMention}/g, `<@${user.id}>`)
