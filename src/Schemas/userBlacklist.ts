360847b9938e14e5c3de22fcbced7d2569996439
scrappie <scr3ppie@gmail.com>
2023-06-07 15:21:48 +0300

    refactor: added interface for UB schema

--- src/Schemas/userBlacklist.ts
+++ src/Schemas/userBlacklist.ts
@@ -1,8 +1,20 @@
 import { model, Schema } from 'mongoose';
 
-export const UserBlacklist = model(
+interface UBInterface {
+	/** Indicates if the user is blacklisted or not. */
+	isBlacklisted: boolean;
+	/** The ID of the user. */
+	userId: string;
+	/** The reason the user is blacklisted for. */
+	reason: string;
+	/** The time when the user was blacklisted. */
+	time: number;
+}
+
+export const UserBlacklist = model<UBInterface>(
 	'UBlacklist',
 	new Schema({
+		isBlacklisted: Boolean,
 		userId: String,
 		reason: String,
 		time: Number,
