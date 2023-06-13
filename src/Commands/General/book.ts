9c3e184bef1bdac62da94e23f460bfc0d2bfa32e
scrappie <scr3ppie@gmail.com>
2023-06-13 17:23:24 +0300

    feat: ported bookinfo + gameinfo + manga

---
+++ src/Commands/General/book.ts
@@ -0,0 +1,76 @@
+import {
+	EmbedBuilder,
+	ApplicationCommandOptionType,
+	ChatInputCommandInteraction,
+} from 'discord.js';
+import { Discord, Guard, Slash, SlashOption } from 'discordx';
+import { RateLimit, TIME_UNIT } from '@discordx/utilities';
+import { GBooksAPI } from '../../Utils/APIs/gBooksAPI.js';
+
+@Discord()
+export class Book {
+	private embed: EmbedBuilder;
+
+	@Slash({
+		description: 'Get info about a book using Google Books.',
+		name: 'book',
+	})
+	@Guard(
+		RateLimit(TIME_UNIT.seconds, 30, {
+			message: '🔹 | You are currently rate limited. Try again at {until}.',
+			ephemeral: true,
+		}),
+	)
+	async book(
+		@SlashOption({
+			name: 'title',
+			description: 'Provide the name of the book.',
+			type: ApplicationCommandOptionType.String,
+			required: true,
+		})
+			title: string,
+			interaction: ChatInputCommandInteraction,
+	) {
+		const bookAPI = new GBooksAPI(interaction);
+		const book = await bookAPI.fetchBook(title);
+		this.embed = new EmbedBuilder().setColor('Blurple').setTimestamp();
+
+		return interaction.reply({
+			embeds: [
+				this.embed
+					.setTitle(book.title)
+					.setDescription(book.description)
+					.addFields(
+						{
+							name: 'Categories',
+							value: `> ${book.categories}`,
+						},
+						{
+							name: 'Written by',
+							value: `> ${book.authors}`,
+							inline: true,
+						},
+						{
+							name: 'Published by',
+							value: `> ${book.publisher}`,
+							inline: true,
+						},
+						{
+							name: 'Pages',
+							value: `> ${book.pageCount}`,
+							inline: true,
+						},
+						{
+							name: 'Released',
+							value: `> <t:${book.publishedDateUnix}>`,
+							inline: true,
+						},
+					)
+					.setFooter({
+						text: 'This information has been brought to you by the Google Books API.',
+					})
+					.setThumbnail(book.coverImage.thumbnail),
+			],
+		});
+	}
+}
---
+++ src/Commands/General/manga.ts
@@ -0,0 +1,72 @@
+import {
+	EmbedBuilder,
+	ApplicationCommandOptionType,
+	ChatInputCommandInteraction,
+} from 'discord.js';
+import { Discord, Guard, Slash, SlashOption } from 'discordx';
+import { RateLimit, TIME_UNIT } from '@discordx/utilities';
+import { KitsuAPI } from '../../Utils/APIs/kitsuAPI.js';
+
+@Discord()
+export class Manga {
+	private readonly embed: EmbedBuilder;
+
+	constructor() {
+		this.embed = new EmbedBuilder().setColor('Blurple').setTimestamp();
+	}
+
+	@Slash({
+		description: 'Get info about a manga using Kitsu.io.',
+		name: 'manga',
+	})
+	@Guard(
+		RateLimit(TIME_UNIT.seconds, 30, {
+			message: '🔹 | You are currently rate limited. Try again at {until}.',
+			ephemeral: true,
+		}),
+	)
+	async manga(
+		@SlashOption({
+			name: 'title',
+			description: 'Provide the name of the manga.',
+			type: ApplicationCommandOptionType.String,
+			required: true,
+		})
+			title: string,
+			interaction: ChatInputCommandInteraction,
+	) {
+		const kitsu = new KitsuAPI(interaction);
+		const manga = await kitsu.fetchManga(title);
+
+		return interaction.reply({
+			embeds: [
+				this.embed
+					.setTitle(manga.titles.en_us)
+					.setThumbnail(manga.posterImage.original)
+					.setDescription(manga.synopsis)
+					.addFields(
+						{
+							name: 'Premiered on',
+							value: `<t:${manga.startDateUnix}>`,
+							inline: true,
+						},
+						{
+							name: 'Ended on',
+							value: `<t:${manga.endDateUnix}>`,
+							inline: true,
+						},
+						{
+							name: 'Japanese Title',
+							value: `${manga.titles.ja_JP}` || 'Unknown.',
+							inline: true,
+						},
+						{
+							name: 'Status',
+							value: manga.status,
+							inline: true,
+						},
+					),
+			],
+		});
+	}
+}
