5a346309596ca663211c8893f65b08c05418728a
scrappie <scr3ppie@gmail.com>
2023-06-13 17:19:24 +0300

    feat: ported announcements

---
+++ src/Commands/General/announce.ts
@@ -0,0 +1,59 @@
+import {
+	Role,
+	TextChannel,
+	EmbedBuilder,
+	PermissionFlagsBits,
+	ApplicationCommandOptionType,
+	ChatInputCommandInteraction,
+} from 'discord.js';
+import { Discord, Slash, SlashOption } from 'discordx';
+
+@Discord()
+export class Announce {
+	private embed: EmbedBuilder;
+
+	@Slash({
+		name: 'announce',
+		description: 'Send an announcement via the bot.',
+		defaultMemberPermissions: PermissionFlagsBits.ManageGuild,
+	})
+	async announce(
+		@SlashOption({
+			name: 'message',
+			description:
+				'Provide the message you would like to send in the announcement.',
+			type: ApplicationCommandOptionType.String,
+			required: true,
+		})
+		@SlashOption({
+			name: 'channel',
+			description: 'Provide the channel where the announcement will be sent.',
+			type: ApplicationCommandOptionType.Channel,
+			required: true,
+		})
+		@SlashOption({
+			name: 'role',
+			description: 'Mention a role alongside the announcement.',
+			type: ApplicationCommandOptionType.Role,
+			required: false,
+		})
+			message: string,
+			channel: TextChannel,
+			role: Role,
+			interaction: ChatInputCommandInteraction,
+	) {
+		this.embed = new EmbedBuilder().setColor('Blurple').setTimestamp();
+
+		interaction.reply({
+			embeds: [this.embed.setDescription('🔹 | Announcement sent.')],
+			ephemeral: true,
+		});
+
+		if (role)
+			return channel.send({
+				content: `<@${role.id}>`,
+				embeds: [this.embed.setDescription(message)],
+			});
+		return channel.send({ embeds: [this.embed.setDescription(message)] });
+	}
+}
