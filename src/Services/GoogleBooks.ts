312a5141806abe54b29f132a7ceae293125069e0
scrappie <notscrappie@proton.me>
2024-03-17 14:31:14 +0200

    refactor(services): converted gbook interface into type
    - also moved it to the services/googlebooks file.

--- src/Services/GoogleBooks.ts
+++ src/Services/GoogleBooks.ts
@@ -1,8 +1,4 @@
-/**
- * A wrapper for the Google Books API.
- */
-
-import { GBooksInterface } from '@/Interfaces/interfaces.js';
+/** A wrapper for the Google Books API. */
 import { singleton } from 'tsyringe';
 import superagent from 'superagent';
 
@@ -12,7 +8,7 @@ export class GoogleBooks {
 	private readonly apiURL: string = 'https://www.googleapis.com/books/v1/volumes';
 
 	/** Retrieves a book using the provided query. */
-	public async fetchBook(book: string): Promise<GBooksInterface> {
+	public async fetchBook(book: string): Promise<Book> {
 		const res = await superagent.get(`${this.apiURL}?q=${book}`);
 		const bookData = res.body.items?.[0]?.volumeInfo;
 
@@ -22,7 +18,7 @@ export class GoogleBooks {
 		const publishedDate = new Date(bookData?.publishedDate);
 		const publishedDateUnix = Math.floor(publishedDate?.getTime() / 1000);
 
-		const bookInfo: GBooksInterface = {
+		const bookInfo: Book = {
 			title: bookData?.title,
 			description: bookData?.description,
 			authors: mappedAuthors,
@@ -39,4 +35,28 @@ export class GoogleBooks {
 
 		return bookInfo;
 	}
+}
+
+type Book = {
+	/** The title of the book. */
+	title: string;
+	/** The description of the book. */
+	description: string;
+	/** The authors of the book. */
+	authors: string[];
+	/** The publisher of the book. */
+	publisher: string;
+	/** The number of pages that the book has. */
+	pageCount: string;
+	/** The date when the book was published. */
+	publishedDate: string;
+	/** The date when the book was published but in Unix. */
+	publishedDateUnix: number;
+	/** The genres the anime is under. */
+	categories: string[];
+	/** The cover image of the book. */
+	coverImage: {
+		smallThumbnail: string;
+		thumbnail: string;
+	};
 }
