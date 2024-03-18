1589e5f2a8dc9027b401f53deb5f325a0ffc0afe
scrappie <notscrappie@proton.me>
2024-03-18 18:30:40 +0200

    refactor(djxManaged/ready): removed colors, added database check (+ other)
    - Evelyn now exits if it doesn't detect a database URL.
    - Replaced all console.info and console.log methods with the new logger.info and logger.error methods.

--- src/Events/djxManaged/Client/ready.ts
+++ src/Events/djxManaged/Client/ready.ts
@@ -1,25 +1,22 @@
-import { reminderCheck } from '@/Utils/Helpers/reminderUtils.js';
-import { check4Lockdowns } from '../../../Utils/Checks/check4Lockdowns.js';
-import { Evelyn } from '../../../Evelyn.js';
+import { check4Lockdowns } from '@/Utils/Checks/check4Lockdowns.js';
+import { reminderCheck } from '@Helpers/reminderUtils.js';
 import { ActivityType } from 'discord.js';
 import { Discord, Once } from 'discordx';
-import colors from '@colors/colors';
-import { set } from 'mongoose';
+import { Evelyn } from '@Evelyn';
+import { connect } from 'mongoose';
 
 @Discord()
 export class Ready {
-	@Once({
-		event: 'ready',
-	})
+	@Once({ event: 'ready' })
 	async ready([client]: [Evelyn]) {
-		const { magenta, white, green, red } = colors;
 		const { user, config, manager } = client;
 
-		console.info(
-			`${magenta('Discord API')} ${white('· Logged in as')} ${green(
-				`${client.user.tag}`,
-			)}`,
-		);
+		if (!config.database) {
+			client.evieLogger.error('Couldn\'t establish a connection to the database, please check your config.ts file.');
+			return process.exit();
+		}
+
+		client.evieLogger.info(`[Discord API] Logged in as ${client.user.tag}.`);
 
 		user.setPresence({
 			activities: [
@@ -30,28 +27,15 @@ export class Ready {
 			],
 		});
 
-		if (!config.database) {
-			return console.error(
-				`${magenta('Evelyn Notification')} ${white('·')} ${red(
-					'Couldn\'t connect to database, please check your config.ts file.',
-				)}`,
-			);
-		}
-
 		await client.initApplicationCommands();
 		manager.init(client);
 
-		await set('strictQuery', false)
-			.connect(config.database)
+		connect(config.database)
 			.then(() => {
-				console.info(
-					`${magenta('Database')} ${white('·')} ${green(
-						`${client.user.username}`,
-					)} ${white('has successfully connected to the database.')}`,
-				);
+				client.evieLogger.info(`[DB] ${client.user.username} has successfully connected to the database.`);
 			})
 			.catch((err) => {
-				console.log(err);
+				client.evieLogger.error(`[DB] ${err}`);
 			});
 
 		await reminderCheck(client);
